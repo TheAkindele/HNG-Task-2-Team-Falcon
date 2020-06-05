d96eabd73ca1d8b9c04e8f21c917cadf3a0b0437
Muhyideen Akindele <51252833+TheAkindele@users.noreply.github.com>
2020-06-05 23:33:21 +0100

    commiting for task 2 by HNG-03451

--- scripts/HNG-03451.js
+++ scripts/HNG-03451.js
@@ -2,4 +2,4 @@ const name = 'AKINDELE MUHYIDEEN'
 const ID = 'HNG-03451'
 const email = 'akindelemuhyideen@gmail.com'
 
-console.log(`Hello World, this is ${name} with ID ${ID} using javascript for the stage 2 task. ${email} `)
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using javascript for the stage 2 task. ${email} `)
